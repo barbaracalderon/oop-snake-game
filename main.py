cd252c9314de68051111c44b9af0249a07b2c427
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-04-14 11:10:35 -0300

    Added collision to the tail

--- main.py
+++ main.py
@@ -32,5 +32,13 @@ while game_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
         scoreboard.game_over()
+    # Detect collision with tail.
+    for segment in snake.snake:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
