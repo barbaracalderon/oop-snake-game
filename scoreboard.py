540068272b5f423cfa909b8f97727553d2ab82f7
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-19 15:14:56 -0300

    Added new method of  keeping track of highest score in the game and changed names to keep it simple

--- scoreboard.py
+++ scoreboard.py
@@ -9,19 +9,24 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color('white')
         self.penup()
-        self.score = 0
-        self.write_scoreboard()
-        self.keep_score()
-
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write('GAME OVER', True, align=ALIGNMENT, font=FONT)
-
-    def write_scoreboard(self):
         self.goto(x=0, y=280)
+        self.score = 0
+        self.high_score = 0
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0                      # reset to zero so it counts again next round
+
+    # def game_over(self):
+    #     self.goto(x=0, y=0)
+    #     self.write('GAME OVER', True, align=ALIGNMENT, font=FONT)
+
+    def update_scoreboard(self):
+        self.clear()
         self.write(f'Score: {self.score} ', True, align=ALIGNMENT, font=FONT)
 
-    def keep_score(self):
-        self.clear()
-        self.write_scoreboard()
+    def increase_score(self):
+        self.update_scoreboard()
         self.score += 1
