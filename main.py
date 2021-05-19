6e6fa717a3437fcd0d6f748bd131e7f7b2eea07a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-19 15:15:13 -0300

    Changed names to follow along the changes

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ while game_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.grow()
-        scoreboard.keep_score()
+        scoreboard.increase_score()
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
