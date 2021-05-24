861a9e143a247f74d052e94c1a1cfeeb3e95b9ef
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-24 15:51:51 -0300

    Added data.txt to keep the highest score

--- scoreboard.py
+++ scoreboard.py
@@ -11,11 +11,18 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=280)
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_file()
         self.update_scoreboard()
+
+    def read_file(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        return self.high_score
 
     def reset(self):
         if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
